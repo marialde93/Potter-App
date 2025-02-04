84a7d845ee2bb253dd4e224f0f428552b7aac9e0
marialde93 <marialujanalderete93@gmail.com>
2025-02-04 18:57:00 -0300

    first commit to production

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,16 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+// Importa el CSS de Bootstrap
+import "bootstrap/dist/css/bootstrap.min.css";
+// Importa los íconos de Bootstrap Icons
+import "bootstrap-icons/font/bootstrap-icons.css";
+// Importa el JS de Bootstrap (opcional, solo si usas componentes que requieren JS)
+import "bootstrap/dist/js/bootstrap.bundle.min.js";
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import App from "./App.jsx";
 
-createRoot(document.getElementById('root')).render(
+createRoot(document.getElementById("root")).render(
   <StrictMode>
     <App />
-  </StrictMode>,
-)
+  </StrictMode>
+);
