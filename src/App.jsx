106a6a0b4f2e7c298187f065f1a2cf7367ca5813
marialde93 <marialujanalderete93@gmail.com>
2025-02-04 15:58:46 -0300

    Eliminado Bootstrap del CDN e instalado localmente

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,9 @@
+// Importa el CSS de Bootstrap
+import "bootstrap/dist/css/bootstrap.min.css";
+// Importa los íconos de Bootstrap Icons
+import "bootstrap-icons/font/bootstrap-icons.css";
+// Importa el JS de Bootstrap (opcional, solo si usas componentes que requieren JS)
+import "bootstrap/dist/js/bootstrap.bundle.min.js";
 import { Router } from "../src/Router/Router";
 
 function App() {
