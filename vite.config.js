3f0766b60bc272037c13b9d2d722fb1f99ece650
marialde93 <marialujanalderete93@gmail.com>
2025-02-12 21:21:16 -0300

    Corregida ruta a main.jsx en index.html

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,8 @@
 import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
 
-// https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  base: "/", // Asegúrate de que la base sea correcta
+  build: {
+    outDir: "dist", // Especifica la carpeta de salida
+  },
 });
